e587ecf818156e4121c9bd4be590c0756b602dbb
IoT Lab <verticals.iot.lab@gmail.com>
2023-11-02 12:44:45 +0100

    add datepicker to signup component

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -6,9 +6,14 @@ import { NgForm } from '@angular/forms';
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss']
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
+  maxDate: Date = new Date();
 
   constructor() {}
+
+  ngOnInit(): void {
+    this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
+  }
 
   onSubmit(form: NgForm) {}
 }
