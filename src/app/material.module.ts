3451309467052fa419f304d6963c634cd09cb517
IoT Lab <verticals.iot.lab@gmail.com>
2023-11-02 20:19:55 +0100

    add sidenav to app component

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -6,6 +6,8 @@ import { MatInputModule } from '@angular/material/input';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   imports: [
@@ -15,7 +17,9 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
     MatInputModule,
     MatDatepickerModule,
     MatNativeDateModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatSidenavModule,
   ],
   exports: [
     MatButtonModule,
@@ -24,7 +28,9 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
     MatInputModule,
     MatDatepickerModule,
     MatNativeDateModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatSidenavModule,
   ],
 })
 export class MaterialModule {}
