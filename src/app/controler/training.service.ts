22d651e407081ca59b4b285b0a0b4eed84653ebf
IoT Lab <verticals.iot.lab@gmail.com>
2023-11-04 17:45:49 +0100

    inject training service in new training component

--- src/app/controler/training.service.ts
+++ src/app/controler/training.service.ts
@@ -5,10 +5,14 @@ import { Exercise } from '../model/exercise.model';
   providedIn: 'root',
 })
 export class TrainingService {
-  avaliableExercises: Exercise[] = [
+  private avaliableExercises: Exercise[] = [
     { id: 'crunches', name: 'Crunches', duration: 30, calories: 8 },
     { id: 'touch-toes', name: 'Touch Toes', duration: 180, calories: 15 },
     { id: 'side-lunges', name: 'Side Lunges', duration: 120, calories: 18 },
     { id: 'burpees', name: 'Burpees', duration: 60, calories: 8 },
   ];
+
+  getAvaliableExercises() {
+      return this.avaliableExercises.slice();
+  }
 }
--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,12 +1,21 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { TrainingService } from 'src/app/controler/training.service';
+import { Exercise } from 'src/app/model/exercise.model';
 
 @Component({
   selector: 'app-new-training',
   templateUrl: './new-training.component.html',
   styleUrls: ['./new-training.component.scss']
 })
-export class NewTrainingComponent {
+export class NewTrainingComponent implements OnInit {
   @Output() trainingStart = new EventEmitter<void>();
+  exercises: Exercise[] = [];
+
+  constructor(private trainingService: TrainingService) {}
+  
+  ngOnInit(): void {
+    this.exercises = this.trainingService.getAvaliableExercises();
+  }
 
   onStartTraining() {
     this.trainingStart.emit();
